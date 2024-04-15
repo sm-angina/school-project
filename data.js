d6788b73066c2f1ddb4992fde19fdb2f89c05f06
Sm Angina <bdbakka33@gmail.com>
2024-04-15 22:51:01 +0600

    1st commit

---
+++ data.js
@@ -0,0 +1,211 @@
+const data = [
+    {
+      name: "Ohi Marwa",
+      id: 2046823,
+      roll: "01",
+      image: 'src/image.jpg',
+      details: {
+        father: 'Abdullah Al Mamun',
+        mother: 'Mosammat Shahina Akter',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Istat Nahar Mimmi",
+      id: 2021623,
+      roll: "02",
+      image: 'src/image002.jpg',
+      details: {
+        father: 'Md Yousuf Ali',
+        mother: 'Nurun Naha',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Mst Fowjia Akter",
+      id: 2020923,
+      roll: "03",
+      image: 'src/image003.jpg',
+      details: {
+        father: 'Md Ali Akmut',
+        mother: 'Kamrunnahar',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "FATHEHA SARMIN TANISHA",
+      id: 2022823,
+      roll: "04",
+      image: 'src/image004.jpg',
+      details: {
+        father: 'MOHAMMAD MOKTER HOSSAIN BHUYAN',
+        mother: 'FARHANA AKTAR PANNA',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Md Mahmud Hasan Bhuiyan",
+      id: 2020623,
+      roll: "05",
+      image: 'src/image005.jpg',
+      details: {
+        father: 'Md Belaet Hossain Bhuiyan',
+        mother: 'Shamima Nasrin',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Asma Akter Bristi",
+      id: 2045623,
+      roll: "06",
+      image: 'src/image006.jpg',
+      details: {
+        father: 'Md Anisur Rahman',
+        mother: 'Parvin Begum',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Nabila Islam",
+      id: 2066323,
+      roll: "07",
+      image: 'src/image007.jpg',
+      details: {
+        father: 'Md Nourul Islam Miazee',
+        mother: 'Somi Akter',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Monira Tabassum",
+      id: 2045423,
+      roll: "08",
+      image: 'src/image008.jpg',
+      details: {
+        father: 'Monirul Islam',
+        mother: 'Rousan Akter',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Shah Mohsen Rezan",
+      id: 2019023,
+      roll: "09",
+      image: 'src/image009.jpg',
+      details: {
+        father: 'Md Shahjahan',
+        mother: 'Mahmuda Akter',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Afifa Chowdhury",
+      id: 2076723,
+      roll: "10",
+      image: 'src/image010.jpg',
+      details: {
+        father: 'Md Abdul Qayyum Chowdhury',
+        mother: 'Shahida gum',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Safayet Islam",
+      id: 2047123,
+      roll: "11",
+      image: 'src/image011.jpg',
+      details: {
+        father: 'Mohammad Idris Miah',
+        mother: 'Mrs Surma Sultana',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Fahmida ltana",
+      id: 2022423,
+      roll: "12",
+      image: 'src/image012.jpg',
+      details: {
+        father: 'Abdul Motaleb',
+        mother: 'Shahina Akter',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Ornab Banik",
+      id: 2020723,
+      roll: "13",
+      image: 'src/image013.jpg',
+      details: {
+        father: 'Nantu Banik',
+        mother: 'Monika Banik',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Al Arafat Siyam",
+      id: 2076923,
+      roll: "14",
+      image: 'src/image014.jpg',
+      details: {
+        father: 'Md Halal Uddin',
+        mother: 'Shamsun Nahar',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Jannatul Ferdoush",
+      id: 2022923,
+      roll: "15",
+      image: 'src/image015.jpg',
+      details: {
+        father: 'Shomon Miah',
+        mother: 'Taslima Begum',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Sakibul Alam Khan Fahad",
+      id: 2046523,
+      roll: "16",
+      image: 'src/image016.jpg',
+      details: {
+        father: 'Fakhrul Alam',
+        mother: 'Parvin Akter',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Rimi Akter",
+      id: 2021723,
+      roll: "17",
+      image: 'src/image017.jpg',
+      details: {
+        father: 'Rahomot Ulla',
+        mother: 'Sanowara Begum',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Ommey Aymon Tonni",
+      id: 2022723,
+      roll: "18",
+      image: 'src/image018.jpg',
+      details: {
+        father: 'Md. Tahidullam',
+        mother: 'Nazrin Nahar',
+        religion: 'Islam',
+      }
+    },
+    {
+      name: "Nusrat Jahan Akhi",
+      id: 2077123,
+      roll: "19",
+      image: 'src/image019.jpg',
+      details: {
+        father: 'Md Nasir Uddin',
+        mother: 'Pervin Akhter Panna',
+        religion: 'Islam',
+      }
+    },
+  ];
---
+++ sheet.js
@@ -0,0 +1,168 @@
+let currentIndex = localStorage.getItem("Index") || 0;
+let main = document.querySelector("main");
+loadArticle(data[currentIndex]);
+
+let rightButton = document.querySelector(".next-article-btn"),
+  leftButton = document.querySelector(".previous-article-btn");
+var currRoll, currId;
+
+function loadArticle(data) {
+  main.innerHTML = `<article data-index="0" data-status="visible">
+    <div class="article-main-section article-section">
+        <div>
+            <div>
+                <div class="student-img">
+                    <img src=${data.image}>
+                </div>
+                <div> 
+                    <input class="student-id" value="${data.id}" onkeyup="idIndex()" onfocusout="inputFcsOut(event, currId)"> 
+                </div>
+                <div class="student-details"> 
+                <table>
+                    <tr>
+                        <th>Father's name</th>
+                        <td> ${data.details.father}</td>
+                    </tr>
+                    <tr>
+                        <th>Mother's name</th>
+                        <td> ${data.details.mother}</td>
+                    </tr>
+                    <tr>
+                        <th>Religion</th>
+                        <td> ${data.details.religion}</td>
+                    </tr>
+                    <tr>
+                        <th>Exam</th>
+                        <td> </td>
+                    </tr>
+              </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="article-description-section article-section">
+        <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <strong>Doloribus porro qui odit?</strong> Omnis
+            doloribus accusamus qui quia fugiat praesentium nostrum culpa saepe.Similique eius <strong>nostrum
+                corporis atque,</strong> natus deserunt ad consequuntur <strong>officiis vel consectetur repellat?</strong></p>
+        </div>
+    </div>
+    <div class="article-title-section article-section">
+        <div class="article-title">
+            <div class="article-heading">
+                <h1>${data.name}<h1>
+            </div>
+            <input class="article-heading-icon" value="${data.roll}" onkeyup="rollIndex()" onfocusout="inputFcsOut(event, currRoll)">
+        </div>
+    </div>
+    <div class="article-button-section article-section">
+        <div class="buttons">
+            <i class='bx bx-left-arrow-circle previous-article-btn' onclick="previousArticle()"></i>
+            <i class='bx bx-right-arrow-circle next-article-btn' onclick="newArticle()"></i>
+        </div>
+    </div>
+    </article>`;
+    currId = data.id
+    currRoll = data.roll
+}
+function newArticle() {
+  currentIndex < data.length ? currentIndex++ : currentIndex;
+  loadArticle(data[currentIndex]);
+  titleAnimation();
+  dscAnimation();
+  saveCrnIndex();
+}
+
+function previousArticle() {
+  currentIndex != 0 ? currentIndex-- : currentIndex;
+  loadArticle(data[currentIndex]);
+  titleAnimation();
+  dscAnimation();
+  saveCrnIndex();
+}
+function saveCrnIndex() {
+  localStorage.setItem("Index", currentIndex);
+}
+window.onresize = () => {
+  if (window.innerWidth < 786) {
+    convertIcon();
+  } else {
+    convertText();
+  }
+};
+function titleAnimation() {
+  let title = document.querySelector(".article-title-section");
+  title.style.visibility = "hidden";
+  setTimeout(() => {
+    title.style.animation = "var(--animation)";
+  }, 500);
+  setTimeout(() => {
+    title.style.visibility = "visible";
+  }, 500);
+}
+function dscAnimation() {
+  let dsc = document.querySelector(".article-description-section");
+  dsc.style.visibility = "hidden";
+  setTimeout(() => {
+    dsc.style.animation = "var(--animation)";
+  }, 300);
+  setTimeout(() => {
+    dsc.style.visibility = "visible";
+  }, 700);
+}
+function convertIcon() {
+  document.body.querySelector(
+    "#js_home"
+  ).innerHTML = `<i class='bx bx-home-alt-2' ></i>`;
+  document.body.querySelector(
+    "#js_blog"
+  ).innerHTML = `<i class='bx bx-message-dots'></i>`;
+  document.body.querySelector(
+    "#js_result"
+  ).innerHTML = `<i class='bx bx-notepad active' ></i>`;
+}
+function convertText() {
+  document.body.querySelector("#js_home").innerHTML = "Home";
+  document.body.querySelector("#js_blog").innerHTML = "Blog";
+  document.body.querySelector("#js_result").innerHTML = "Result";
+}
+
+
+
+function rollIndex(e = event) {
+  let input = document.querySelector('.article-heading-icon')
+  let currRoll = input.value
+  if(e.key == "Enter" && input.value < data.length){
+    currentIndex = input.value - 1
+    loadArticle(data[currentIndex])
+  }
+}
+
+function inputFcsOut(e, currValue){
+  e.target.value = currValue
+}
+
+function idIndex(student = data, e = event){
+  if(e.key > 0){
+    setTimeout(()=>{
+      let id = document.querySelector('.student-id')
+        for (let i = 0; i< data.length; i++){
+          if(student[i].id == id.value){
+            currentIndex = i
+            loadArticle(data[currentIndex])
+          }
+        }
+
+    },300)
+  }
+}
+
+
+document.body.onkeyup = arrowKeys;
+function arrowKeys(e) {
+  if (e.key == "ArrowLeft") {
+    leftButton.click();
+  } else if (e.key == "ArrowRight") {
+    rightButton.click();
+  }
+}
